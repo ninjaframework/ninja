2cee8ad7df3f07c9681f8780d2946d97341f4cdd
Raphael A. Bauer <raphael.bauer@devbliss.de>
2012-07-23 11:46:59 +0200

    Now everything must specify a language if they are talking to Lang. Determining the language by some magic does not make sense in a request based setup.

--- ninja-core/src/main/java/ninja/params/Param.java
+++ ninja-core/src/main/java/ninja/params/Param.java
@@ -5,8 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.google.common.annotations.Beta;
-
 /**
  * Just an idea to inject parameters right into the methods...
  * 
