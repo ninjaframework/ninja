160c650de4e2e5fe0eadc6cc40fea3e7f0f4dfad
mreinwarth <mreinwarth@vz.net>
2012-12-06 17:47:02 +0100

    added BodyParserEngineXml

---
+++ ninja-core/src/main/java/ninja/bodyparser/BodyParserEngineXml.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ninja.bodyparser;
+
+import java.io.IOException;
+
+import ninja.Context;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.slf4j.Logger;
+
+import com.fasterxml.jackson.xml.XmlMapper;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class BodyParserEngineXml implements BodyParserEngine {
+    
+    private XmlMapper xmlMapper;
+    
+    private Logger logger;
+    
+
+    @Inject
+    public BodyParserEngineXml(XmlMapper xmlMapper, Logger logger) {
+        this.xmlMapper = xmlMapper;
+        this.logger = logger;
+
+    }
+
+    public <T> T invoke(Context context, Class<T> classOfT) {
+        T t = null;
+
+        try {
+            
+            t = xmlMapper.readValue(IOUtils.toString(context.getReader()), classOfT);
+
+        } catch (JsonParseException e) {
+            logger.error("Error parsing incoming Xml", e);
+        } catch (JsonMappingException e) {
+            logger.error("Error parsing incoming Xml", e);
+        } catch (IOException e) {
+            logger.error("Error parsing incoming Xml", e);
+        }
+
+        return t;
+    }
+
+}
