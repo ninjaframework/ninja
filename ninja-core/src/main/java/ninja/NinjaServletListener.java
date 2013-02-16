5301cd86d04d5aeff79066c6820537d685f63522
zoza <zoza@zoza.ru>
2013-02-17 00:43:47 +0200

    new way we start the Ninja framework

---
+++ ninja-core/src/main/java/ninja/NinjaServletListener.java
@@ -0,0 +1,40 @@
+package ninja;
+
+import javax.servlet.ServletContextEvent;
+
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+/**
+ * define in web.xml:
+ * 
+ * <listener>
+ *   <listener-class>ninja.NinjaServletListener</listener-class>
+ * </listener>
+ *  
+ * @author zoza
+ * 
+ */
+public class NinjaServletListener extends GuiceServletContextListener {
+    
+private NinjaBootstap ninjaBootstap;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ninjaBootstap = new NinjaBootstap(servletContextEvent.getServletContext());
+        ninjaBootstap.boot();
+        super.contextInitialized(servletContextEvent);
+    }
+   
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ninjaBootstap.shutdown();
+        super.contextDestroyed(servletContextEvent);
+    }
+   
+    @Override
+    protected Injector getInjector() {
+        return ninjaBootstap.getInjector();
+    }
+
+}
