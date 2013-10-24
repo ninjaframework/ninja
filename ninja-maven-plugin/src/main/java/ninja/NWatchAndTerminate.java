b252974bb44ee53bce9c3526db552e0ba99080a7
reyez <raphael.andre.bauer@gmail.com>
2013-10-24 23:05:39 +0200

    some more steps towards a working solution...

--- ninja-maven-plugin/src/main/java/ninja/NWatchAndTerminate.java
+++ ninja-maven-plugin/src/main/java/ninja/NWatchAndTerminate.java
@@ -1,250 +1,252 @@
-//package ninja;
+package ninja;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NWatchAndTerminate {
+  
+  RevolverSingleBullet revolver;
+  
+  WatchService watcher;
+
+  AtomicInteger atomicInteger = new AtomicInteger(0);
+  
+    private final Map<WatchKey,Path> keys;
+    private final boolean recursive;
+    private boolean trace = false;
+
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
+    }
+
+    /**
+     * Register the given directory with the WatchService
+     */
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        if (trace) {
+            Path prev = keys.get(key);
+            if (prev == null) {
+                System.out.format("register: %s\n", dir);
+            } else {
+                if (!dir.equals(prev)) {
+                    System.out.format("update: %s -> %s\n", prev, dir);
+                }
+            }
+        }
+        keys.put(key, dir);
+        
+        
+    }
+
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException
+            {
+                register(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * Creates a WatchService and registers the given directory
+     */
+    public NWatchAndTerminate(Path dir, boolean recursive, String clazz, List<String> classpath) throws IOException {
+      
+      
+      this.revolver = new RevolverSingleBullet(clazz, classpath);
+      
+      RestartAfterSomeTimeAndChanges restartAfterSomeTimeAndChanges = new RestartAfterSomeTimeAndChanges(atomicInteger, revolver);
+      restartAfterSomeTimeAndChanges.start();
+      
+        this.watcher = FileSystems.getDefault().newWatchService();
+        this.keys = new HashMap<WatchKey,Path>();
+        this.recursive = recursive;
+
+        if (recursive) {
+            System.out.format("Scanning1 %s ...\n", dir);
+            registerAll(dir);
+            System.out.println("Done.");
+        } else {
+            System.out.format("Scanning non recursive %s ...\n", dir);
+            register(dir);
+        }
+
+        // enable trace after initial registration
+        this.trace = true;
+        
+        
+        
+    }
+
+    /**
+     * Process all events for keys queued to the watcher
+     */
+    public void processEvents() {
+      
+      
+        for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+                System.err.println("WatchKey not recognized!!");
+                continue;
+            }
+            
+            
+            
+            
+            //revolver.pullTheTrigger();
+            System.out.println("reload...");
+            atomicInteger.incrementAndGet();
+            
+
+            for (WatchEvent<?> event: key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                // TBD - provide example of how OVERFLOW event is handled
+                if (kind == OVERFLOW) {
+                    continue;
+                }
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                // print out event
+                System.out.format("%s: %s\n", event.kind().name(), child);
+                
+
+                
+                //revolver.pullTheTrigger();
+                
+
+                // if directory is created, and watching recursively, then
+                // register it and its sub-directories
+                if (recursive && (kind == ENTRY_CREATE)) {
+                    try {
+                        if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
+                            registerAll(child);
+                        }
+                    } catch (IOException x) {
+                        // ignore to keep sample readable
+                    }
+
+                   
+                }
+                
+                
+                
+            }
+
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+                
+            }
+            
+        }
+     
+        
+    }
+
+
+//    public static void main(String[] args) throws IOException {
 //
-//import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
-//import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-//import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-//import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-//import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 //
-//import java.io.IOException;
-//import java.nio.file.FileSystems;
-//import java.nio.file.FileVisitResult;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.SimpleFileVisitor;
-//import java.nio.file.WatchEvent;
-//import java.nio.file.WatchKey;
-//import java.nio.file.WatchService;
-//import java.nio.file.attribute.BasicFileAttributes;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.concurrent.atomic.AtomicInteger;
-//
-//public class NWatchAndTerminate {
-//  
-//  RevolverSingleBullet revolver;
-//  
-//  WatchService watcher;
-//
-//  AtomicInteger atomicInteger = new AtomicInteger(0);
-//  
-//    private final Map<WatchKey,Path> keys;
-//    private final boolean recursive;
-//    private boolean trace = false;
-//
-//    @SuppressWarnings("unchecked")
-//    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-//        return (WatchEvent<T>)event;
+//        // register directory and process its events
+//        Path dir = Paths.get("target/classes");
+//        new NWatchAndTerminate(dir, true).processEvents();
 //    }
-//
-//    /**
-//     * Register the given directory with the WatchService
-//     */
-//    private void register(Path dir) throws IOException {
-//        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-//        if (trace) {
-//            Path prev = keys.get(key);
-//            if (prev == null) {
-//                System.out.format("register: %s\n", dir);
-//            } else {
-//                if (!dir.equals(prev)) {
-//                    System.out.format("update: %s -> %s\n", prev, dir);
-//                }
-//            }
-//        }
-//        keys.put(key, dir);
-//        
-//        
-//    }
-//
-//    /**
-//     * Register the given directory, and all its sub-directories, with the
-//     * WatchService.
-//     */
-//    private void registerAll(final Path start) throws IOException {
-//        // register directory and sub-directories
-//        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-//            @Override
-//            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-//                throws IOException
-//            {
-//                register(dir);
-//                return FileVisitResult.CONTINUE;
-//            }
-//        });
-//    }
-//
-//    /**
-//     * Creates a WatchService and registers the given directory
-//     */
-//    public NWatchAndTerminate(Path dir, boolean recursive, Class clazz, String classpath) throws IOException {
-//      
-//      
-//      this.revolver = new RevolverSingleBullet(clazz, classpath);
-//      
-//      RestartAfterSomeTimeAndChanges restartAfterSomeTimeAndChanges = new RestartAfterSomeTimeAndChanges(atomicInteger, revolver);
-//      restartAfterSomeTimeAndChanges.start();
-//      
-//        this.watcher = FileSystems.getDefault().newWatchService();
-//        this.keys = new HashMap<WatchKey,Path>();
-//        this.recursive = recursive;
-//
-//        if (recursive) {
-//            System.out.format("Scanning %s ...\n", dir);
-//            registerAll(dir);
-//            System.out.println("Done.");
-//        } else {
-//            register(dir);
-//        }
-//
-//        // enable trace after initial registration
-//        this.trace = true;
-//        
-//        
-//        
-//    }
-//
-//    /**
-//     * Process all events for keys queued to the watcher
-//     */
-//    public void processEvents() {
-//      
-//      
-//        for (;;) {
-//
-//            // wait for key to be signalled
-//            WatchKey key;
-//            try {
-//                key = watcher.take();
-//            } catch (InterruptedException x) {
-//                return;
-//            }
-//
-//            Path dir = keys.get(key);
-//            if (dir == null) {
-//                System.err.println("WatchKey not recognized!!");
-//                continue;
-//            }
-//            
-//            
-//            
-//            
-//            //revolver.pullTheTrigger();
-//            System.out.println("reload...");
-//            atomicInteger.incrementAndGet();
-//            
-//
-//            for (WatchEvent<?> event: key.pollEvents()) {
-//                WatchEvent.Kind kind = event.kind();
-//
-//                // TBD - provide example of how OVERFLOW event is handled
-//                if (kind == OVERFLOW) {
-//                    continue;
-//                }
-//
-//                // Context for directory entry event is the file name of entry
-//                WatchEvent<Path> ev = cast(event);
-//                Path name = ev.context();
-//                Path child = dir.resolve(name);
-//
-//                // print out event
-//                System.out.format("%s: %s\n", event.kind().name(), child);
-//                
-//
-//                
-//                //revolver.pullTheTrigger();
-//                
-//
-//                // if directory is created, and watching recursively, then
-//                // register it and its sub-directories
-//                if (recursive && (kind == ENTRY_CREATE)) {
-//                    try {
-//                        if (Files.isDirectory(child, NOFOLLOW_LINKS)) {
-//                            registerAll(child);
-//                        }
-//                    } catch (IOException x) {
-//                        // ignore to keep sample readable
-//                    }
-//
-//                   
-//                }
-//                
-//                
-//                
-//            }
-//
-//            // reset key and remove from set if directory no longer accessible
-//            boolean valid = key.reset();
-//            if (!valid) {
-//                keys.remove(key);
-//
-//                // all directories are inaccessible
-//                if (keys.isEmpty()) {
-//                    break;
-//                }
-//                
-//            }
-//            
-//        }
-//     
-//        
-//    }
-//
-//
-////    public static void main(String[] args) throws IOException {
-////
-////
-////        // register directory and process its events
-////        Path dir = Paths.get("target/classes");
-////        new NWatchAndTerminate(dir, true).processEvents();
-////    }
-//    
-//    
-//    
-//
-//  private static class RestartAfterSomeTimeAndChanges extends Thread {
-//
-//
-//    AtomicInteger atomicInteger;
-//    private RevolverSingleBullet revolver;
-//    
-//    private RestartAfterSomeTimeAndChanges(
-//        AtomicInteger atomicInteger,
-//        RevolverSingleBullet revolver) {
-//      this.atomicInteger = atomicInteger;
-//      this.revolver = revolver;
-//      
-//    }
-//
-//    @Override
-//    public void run() {
-//      
-//      while(true) {
-//        try {
-//          
-//          sleep(100);
-//          if (atomicInteger.get() > 0) {
-//            atomicInteger.set(0);
-//            
-//            System.out.println("restarting really...");
-//            revolver.pullTheTrigger();
-//            
-//            
-//            
-//            
-//            
-//          }
-//          
-//          
-//        } catch (InterruptedException e) {
-//          // TODO Auto-generated catch block
-//          e.printStackTrace();
-//        }
-//      }
-//      
-//    }
-//  }
-//
-//  
-//  
-//}
+    
+    
+    
+
+  private static class RestartAfterSomeTimeAndChanges extends Thread {
+
+
+    AtomicInteger atomicInteger;
+    private RevolverSingleBullet revolver;
+    
+    private RestartAfterSomeTimeAndChanges(
+        AtomicInteger atomicInteger,
+        RevolverSingleBullet revolver) {
+      this.atomicInteger = atomicInteger;
+      this.revolver = revolver;
+      
+    }
+
+    @Override
+    public void run() {
+      
+      while(true) {
+        try {
+          
+          sleep(100);
+          if (atomicInteger.get() > 0) {
+            atomicInteger.set(0);
+            
+            System.out.println("restarting really...");
+            revolver.pullTheTrigger();
+            
+            
+            
+            
+            
+          }
+          
+          
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+      
+    }
+  }
+
+  
+  
+}
--- ninja-maven-plugin/src/main/java/ninja/RevolverSingleBullet.java
+++ ninja-maven-plugin/src/main/java/ninja/RevolverSingleBullet.java
@@ -1,127 +1,134 @@
-//package ninja;
-//
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.URL;
-//import java.net.URLClassLoader;
-//
-//public class RevolverSingleBullet {
-//
-//    Process process0;
-//
-//    // Main class that will be used to load...
-//    private Class klass;
-//    
-//    private String classpath;
-//
-//    public RevolverSingleBullet(Class klass, String classpath) {
-//        this.klass = klass;
-//        this.classpath = classpath;
-//
-//        // initial startup
-//        try {
-//            process0 = exec(klass);
-//
-//        } catch (IOException | InterruptedException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//
-//        }
-//
-//        Runtime.getRuntime().addShutdownHook(new Thread() {
-//            @Override
-//            public void run() {
-//                System.out.println("Inside Add Shutdown Hook");
-//                process0.destroy();
-//
-//            }
-//        });
-//        System.out.println("Shut Down Hook Attached.");
-//
-//    }
-//
-//    public synchronized void pullTheTrigger() {
-//
-//        try {
-//            process0.destroy();
-//            process0.waitFor();
-//            process0 = exec(klass);
-//        } catch (InterruptedException | IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    public Process exec(Class klass) throws IOException,
-//            InterruptedException {
-//
-//        ClassLoader cl = ClassLoader.getSystemClassLoader();
-//
-//        URL[] urls = ((URLClassLoader) cl).getURLs();
-//
-//        for (URL url : urls) {
-//            System.out.println(url.getFile());
-//        }
-//
-//        String javaHome = System.getProperty("java.home");
-//        String javaBin = javaHome + File.separator + "bin" + File.separator
-//                + "java";
-//        // String classpath = System.getProperty("java.class.path") + ":" +
-//        // "/Users/ra/bibliothek/coden/workspace_t35/ninja-maven-plugin/target/classes/";
-//
-//        // String className = klass.getCanonicalName();
-//
-// 
-//        System.out.println("path: " + classpath);
-//        String className = klass.getCanonicalName();
-//        //
-//        ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classpath,
-//                className);
-//        //
-//
-//        builder.redirectErrorStream(true);
-//
-//        Process process = builder.start();
-//
-//        // StreamGobbler errorGobbler = new StreamGobbler(
-//        // process.getErrorStream(), "ERROR");
-//        //
-//        // // any output?
-//        StreamGobbler outputGobbler = new StreamGobbler(
-//                process.getInputStream(), "OUTPUT");
-//        //
-//        // // start gobblers
-//        outputGobbler.start();
-//        // errorGobbler.start();
-//
-//        return process;
-//    }
-//
-//    private static class StreamGobbler extends Thread {
-//        InputStream is;
-//        String type;
-//
-//        private StreamGobbler(InputStream is, String type) {
-//            this.is = is;
-//            this.type = type;
-//        }
-//
-//        @Override
-//        public void run() {
-//            try {
-//                InputStreamReader isr = new InputStreamReader(is);
-//                BufferedReader br = new BufferedReader(isr);
-//                String line = null;
-//                while ((line = br.readLine()) != null)
-//                    System.out.println(type + "> " + line);
-//            } catch (IOException ioe) {
-//                ioe.printStackTrace();
-//            }
-//        }
-//    }
-//
-//}
+package ninja;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
+import com.google.common.base.Joiner;
+
+public class RevolverSingleBullet {
+
+    Process process0;
+
+    // Main class that will be used to load...
+    private String klass;
+    
+    private List<String> classpath;
+
+    public RevolverSingleBullet(String klass, List<String> classpath) {
+        this.klass = klass;
+        this.classpath = classpath;
+
+        // initial startup
+        try {
+            process0 = exec(klass);
+
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Inside Add Shutdown Hook");
+                process0.destroy();
+
+            }
+        });
+        System.out.println("Shut Down Hook Attached.");
+
+    }
+
+    public synchronized void pullTheTrigger() {
+
+        try {
+            process0.destroy();
+            process0.waitFor();
+            process0 = exec(klass);
+        } catch (InterruptedException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public Process exec(String klass) throws IOException,
+            InterruptedException {
+
+
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome + File.separator + "bin" + File.separator
+                + "java";
+        // String classpath = System.getProperty("java.class.path") + ":" +
+        // "/Users/ra/bibliothek/coden/workspace_t35/ninja-maven-plugin/target/classes/";
+
+        // String className = klass.getCanonicalName();
+
+ 
+        
+        
+        String pathSeparator = System.getProperty("path.separator");
+        
+        String classpathAsString = Joiner.on(pathSeparator).join(classpath);
+        System.out.println("path: " + classpathAsString);
+        
+        System.out.println("javaHome: " + javaBin);
+        System.out.println("klass: " + klass);
+        //
+        ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classpathAsString,
+                klass);
+        builder.directory(new File("/Users/ra/bibliothek/coden/workspace_t35/ninja/ninja-maven-plugin/integration-test"));
+    
+        
+        //
+
+        builder.redirectErrorStream(true);
+
+        
+        Process process = builder.start();
+        // StreamGobbler errorGobbler = new StreamGobbler(
+        // process.getErrorStream(), "ERROR");
+        //
+        // // any output?
+        StreamGobbler outputGobbler = new StreamGobbler(
+                process.getInputStream(), "OUTPUT");
+        //
+        // // start gobblers
+        outputGobbler.start();
+        // errorGobbler.start();
+
+        return process;
+    }
+
+    private static class StreamGobbler extends Thread {
+        InputStream is;
+        String type;
+
+        private StreamGobbler(InputStream is, String type) {
+            this.is = is;
+            this.type = type;
+        }
+
+        @Override
+        public void run() {
+            try {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(type + "> " + line);
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+}
