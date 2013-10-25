c7e8a5fb9efdc4f30886f637cf6bf66e7488b6c7
Aliaksei Nikuliak <nobullet@gmail.com>
2013-10-25 12:54:35 -0700

    Adding junit and integration tests for JSONP

--- ninja-core/src/test/java/ninja/template/TemplateEngineJsonPTest.java
+++ ninja-core/src/test/java/ninja/template/TemplateEngineJsonPTest.java
@@ -83,4 +83,15 @@ public class TemplateEngineJsonPTest {
         String jsonp = new String(outputStream.toByteArray(), "UTF-8");
         assertEquals(TemplateEngineJsonP.DEFAULT_CALLBACK_PARAMETER_VALUE + "([123])", jsonp);
     }
+
+    @Test
+    public void testBadCallbackNameFlow() throws IOException {
+        when(context.getParameter("callback", TemplateEngineJsonP.DEFAULT_CALLBACK_PARAMETER_VALUE)).thenReturn(".callback");
+
+        TemplateEngineJsonP jsonpEngine = new TemplateEngineJsonP(logger, objectMapper, properties);
+        jsonpEngine.invoke(context, result);
+
+        String jsonp = new String(outputStream.toByteArray(), "UTF-8");
+        assertEquals(TemplateEngineJsonP.DEFAULT_CALLBACK_PARAMETER_VALUE + "([123])", jsonp);
+    }
 }
