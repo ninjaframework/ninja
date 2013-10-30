978b3cbee6aea440d1aab0d6b85bd27344db825c
reyez <raphael.andre.bauer@gmail.com>
2013-10-30 21:06:33 +0100

    A lot more improvements.

--- ninja-maven-plugin/src/main/java/ninja/NWatchAndTerminate.java
+++ ninja-maven-plugin/src/main/java/ninja/NWatchAndTerminate.java
@@ -21,28 +21,35 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class NWatchAndTerminate {
-  
-  RevolverSingleBullet revolver;
-  
-  WatchService watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  AtomicInteger atomicInteger = new AtomicInteger(0);
-  
-    private final Map<WatchKey,Path> keys;
+public class NWatchAndTerminate {
+    
+    private static Logger logger = LoggerFactory.getLogger(NWatchAndTerminate.class);
+
+    
+    RevolverSingleBullet revolver;
+
+    WatchService watcher;
+
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private final Map<WatchKey, Path> keys;
     private final boolean recursive;
     private boolean trace = false;
 
     @SuppressWarnings("unchecked")
     static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-        return (WatchEvent<T>)event;
+        return (WatchEvent<T>) event;
     }
 
     /**
      * Register the given directory with the WatchService
      */
     private void register(Path dir) throws IOException {
-        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+        WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,
+                ENTRY_MODIFY);
         if (trace) {
             Path prev = keys.get(key);
             if (prev == null) {
@@ -54,8 +61,7 @@ public class NWatchAndTerminate {
             }
         }
         keys.put(key, dir);
-        
-        
+
     }
 
     /**
@@ -66,9 +72,9 @@ public class NWatchAndTerminate {
         // register directory and sub-directories
         Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
             @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                throws IOException
-            {
+            public FileVisitResult preVisitDirectory(Path dir,
+                                                     BasicFileAttributes attrs)
+                    throws IOException {
                 register(dir);
                 return FileVisitResult.CONTINUE;
             }
@@ -78,16 +84,19 @@ public class NWatchAndTerminate {
     /**
      * Creates a WatchService and registers the given directory
      */
-    public NWatchAndTerminate(Path dir, boolean recursive, String clazz, List<String> classpath) throws IOException {
-      
-      
-      this.revolver = new RevolverSingleBullet(clazz, classpath);
-      
-      RestartAfterSomeTimeAndChanges restartAfterSomeTimeAndChanges = new RestartAfterSomeTimeAndChanges(atomicInteger, revolver);
-      restartAfterSomeTimeAndChanges.start();
-      
+    public NWatchAndTerminate(Path dir,
+                              boolean recursive,
+                              String clazz,
+                              List<String> classpath) throws IOException {
+
+        this.revolver = new RevolverSingleBullet(clazz, classpath);
+
+        RestartAfterSomeTimeAndChanges restartAfterSomeTimeAndChanges = new RestartAfterSomeTimeAndChanges(
+                atomicInteger, revolver);
+        restartAfterSomeTimeAndChanges.start();
+
         this.watcher = FileSystems.getDefault().newWatchService();
-        this.keys = new HashMap<WatchKey,Path>();
+        this.keys = new HashMap<WatchKey, Path>();
         this.recursive = recursive;
 
         if (recursive) {
@@ -101,17 +110,14 @@ public class NWatchAndTerminate {
 
         // enable trace after initial registration
         this.trace = true;
-        
-        
-        
+
     }
 
     /**
      * Process all events for keys queued to the watcher
      */
     public void processEvents() {
-      
-      
+
         for (;;) {
 
             // wait for key to be signalled
@@ -127,16 +133,12 @@ public class NWatchAndTerminate {
                 System.err.println("WatchKey not recognized!!");
                 continue;
             }
-            
-            
-            
-            
-            //revolver.pullTheTrigger();
+
+            // revolver.pullTheTrigger();
             System.out.println("reload...");
             atomicInteger.incrementAndGet();
-            
 
-            for (WatchEvent<?> event: key.pollEvents()) {
+            for (WatchEvent<?> event : key.pollEvents()) {
                 WatchEvent.Kind kind = event.kind();
 
                 // TBD - provide example of how OVERFLOW event is handled
@@ -151,11 +153,8 @@ public class NWatchAndTerminate {
 
                 // print out event
                 System.out.format("%s: %s\n", event.kind().name(), child);
-                
 
-                
-                //revolver.pullTheTrigger();
-                
+                // revolver.pullTheTrigger();
 
                 // if directory is created, and watching recursively, then
                 // register it and its sub-directories
@@ -168,11 +167,8 @@ public class NWatchAndTerminate {
                         // ignore to keep sample readable
                     }
 
-                   
                 }
-                
-                
-                
+
             }
 
             // reset key and remove from set if directory no longer accessible
@@ -184,69 +180,46 @@ public class NWatchAndTerminate {
                 if (keys.isEmpty()) {
                     break;
                 }
-                
+
             }
-            
+
         }
-     
-        
+
     }
 
+    private static class RestartAfterSomeTimeAndChanges extends Thread {
 
-//    public static void main(String[] args) throws IOException {
-//
-//
-//        // register directory and process its events
-//        Path dir = Paths.get("target/classes");
-//        new NWatchAndTerminate(dir, true).processEvents();
-//    }
-    
-    
-    
+        AtomicInteger atomicInteger;
+        private RevolverSingleBullet revolver;
 
-  private static class RestartAfterSomeTimeAndChanges extends Thread {
+        private RestartAfterSomeTimeAndChanges(AtomicInteger atomicInteger,
+                                               RevolverSingleBullet revolver) {
+            this.atomicInteger = atomicInteger;
+            this.revolver = revolver;
 
-
-    AtomicInteger atomicInteger;
-    private RevolverSingleBullet revolver;
-    
-    private RestartAfterSomeTimeAndChanges(
-        AtomicInteger atomicInteger,
-        RevolverSingleBullet revolver) {
-      this.atomicInteger = atomicInteger;
-      this.revolver = revolver;
-      
-    }
-
-    @Override
-    public void run() {
-      
-      while(true) {
-        try {
-          
-          sleep(100);
-          if (atomicInteger.get() > 0) {
-            atomicInteger.set(0);
-            
-            System.out.println("restarting really...");
-            revolver.pullTheTrigger();
-            
-            
-            
-            
-            
-          }
-          
-          
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
         }
-      }
-      
-    }
-  }
 
-  
-  
+        @Override
+        public void run() {
+
+            while (true) {
+                try {
+
+                    sleep(50);
+                    if (atomicInteger.get() > 0) {
+                        atomicInteger.set(0);
+
+                        System.out.println("restarting really...");
+                        revolver.pullTheTrigger();
+
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
 }
--- ninja-maven-plugin/src/main/java/ninja/NinjaRunMojo.java
+++ ninja-maven-plugin/src/main/java/ninja/NinjaRunMojo.java
@@ -6,30 +6,17 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
-import org.eclipse.aether.DefaultRepositorySystemSession;
-import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.RepositorySystemSession;
-import org.eclipse.aether.artifact.Artifact;
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.graph.DependencyFilter;
-import org.eclipse.aether.repository.LocalRepository;
-import org.eclipse.aether.repository.RemoteRepository;
-import org.eclipse.aether.resolution.ArtifactResult;
-import org.eclipse.aether.resolution.DependencyRequest;
-import org.eclipse.aether.resolution.DependencyResolutionException;
-import org.eclipse.aether.util.artifact.JavaScopes;
-import org.eclipse.aether.util.filter.DependencyFilterUtils;
+import org.eclipse.aether.util.artifact.ArtifactIdUtils;
 
 import com.google.common.collect.Lists;
 
 /**
- * Finds all artifacts by names in the current project.
+ * Starts Ninja's hot relaod dev mode.
  * 
  * @goal run
  * @phase test
@@ -37,251 +24,59 @@ import com.google.common.collect.Lists;
  * @requiresDependencyResolution compile
  */
 public class NinjaRunMojo extends AbstractMojo {
-
+    
+    public final String CLASSES_DIRECTORY = File.separator + "target" + File.separator + "classes";
+    
+    public final String NINJA_JETTY_CLASS_AND_CLASSPATH = "ninja.standalone.NinjaJetty";
+    
     /**
      * @parameter expression="${project}"
      * @required
      * @readonly
      */
-    protected MavenProject project;
-
-    /**
-     * The entry point to Aether, i.e. the component doing all the work.
-     * 
-     * @component
+    protected MavenProject mavenProject;
+    
+    /** 
+     * @parameter default-value="${plugin.artifacts}" 
      */
-    private RepositorySystem repoSystem;
-
-    /**
-     * The current repository/network configuration of Maven.
-     * 
-     * @parameter default-value="${repositorySystemSession}"
-     * @readonly
-     */
-    private RepositorySystemSession repoSession;
-
-    /**
-     * The project's remote repositories to use for the resolution of project
-     * dependencies.
-     * 
-     * @parameter default-value="${project.remoteProjectRepositories}"
-     * @readonly
-     */
-    private List<RemoteRepository> projectRepos;
-
-    /**
-     * The project's remote repositories to use for the resolution of plugins
-     * and their dependencies.
-     * 
-     * @parameter default-value="${project.remotePluginRepositories}"
-     * @readonly
-     */
-    private List<RemoteRepository> pluginRepos;
+    private List<Artifact> pluginArtifacts;
 
     @Override
     public void execute() throws MojoExecutionException {
+        
+        String completeClassesDir = new File("").getAbsolutePath() + CLASSES_DIRECTORY;
 
         System.out
                 .println("------------------------------------------------------------");
-        //System.out.println(ResolveTransitiveDependencies.class.getSimpleName());
         
         List<String> classpathItems = Lists.newArrayList();
-        classpathItems.add(".");
+        //classpathItems.add(".");
+
+        classpathItems.add(completeClassesDir);
         
-        classpathItems.add("/Users/ra/bibliothek/coden/workspace_t35/ninja/ninja-maven-plugin/integration-test/target/classes");
+        //Artifact artifact = Artifact
         
-        for (org.apache.maven.artifact.Artifact artifact : project.getArtifacts()) {
-            System.out.println("artifact : " + artifact.getArtifactId());
-            System.out.println("artifact file : " + artifact.getFile());
+        for (org.apache.maven.artifact.Artifact artifact : pluginArtifacts) {
+            //classpathItems.add(artifact.getFile().toString());  
+        } 
+        
+        for (org.apache.maven.artifact.Artifact artifact : mavenProject.getArtifacts()) {
             classpathItems.add(artifact.getFile().toString());           
-        }
+        }       
         
-        
-        Path path = FileSystems.getDefault().getPath("/Users/ra/bibliothek/coden/workspace_t35/ninja/ninja-maven-plugin/integration-test/target/classes");
+        Path path = FileSystems.getDefault().getPath(completeClassesDir);
         
         try {
             NWatchAndTerminate nWatchAndTerminate = new NWatchAndTerminate(
                     path,
                     true, 
-                    "ninja.standalone.NinjaJetty", 
+                    NINJA_JETTY_CLASS_AND_CLASSPATH, 
                     classpathItems);
             nWatchAndTerminate.processEvents();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-        
- 
-     
-
-//        RepositorySystem system = repoSystem;
-//
-//        RepositorySystemSession session = newRepositorySystemSession(system);
-//        
-//        Artifact artifact = new DefaultArtifact(
-//                String.format("%s:%s:%s", project.getGroupId(), project.getArtifactId(), project.getVersion()));
-//
-//        //RemoteRepository repo = newCentralRepository();
-//
-//        DependencyFilter classpathFlter = DependencyFilterUtils
-//                .classpathFilter(JavaScopes.COMPILE);
-//
-//        CollectRequest collectRequest = new CollectRequest();
-//        collectRequest.setRoot(new Dependency(artifact, JavaScopes.COMPILE));
-//        
-//        for (RemoteRepository remoteRepository : projectRepos) {
-//            collectRequest.addRepository(remoteRepository);
-//        }
-//        
-//
-//        DependencyRequest dependencyRequest = new DependencyRequest(
-//                collectRequest, classpathFlter);
-//
-//        List<ArtifactResult> artifactResults;
-//        try {
-//            artifactResults = system.resolveDependencies(
-//                    session, dependencyRequest).getArtifactResults();
-//            
-//            for (ArtifactResult artifactResult : artifactResults) {
-//                System.out.println(artifactResult.getArtifact() + " resolved to "
-//                        + artifactResult.getArtifact().getFile());
-//            }
-//            
-//        } catch (DependencyResolutionException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-
-
-
     }
-
-//    public static RepositorySystem newRepositorySystem() {
-//        return org.eclipse.aether.examples.manual.ManualRepositorySystemFactory
-//                .newRepositorySystem();
-//        // return
-//        // org.eclipse.aether.examples.guice.GuiceRepositorySystemFactory.newRepositorySystem();
-//        // return
-//        // org.eclipse.aether.examples.plexus.PlexusRepositorySystemFactory.newRepositorySystem();
-//    }
-
-    public static DefaultRepositorySystemSession newRepositorySystemSession(RepositorySystem system) {
-        DefaultRepositorySystemSession session = MavenRepositorySystemUtils
-                .newSession();
-
-        LocalRepository localRepo = new LocalRepository("target/local-repo");
-        session.setLocalRepositoryManager(system.newLocalRepositoryManager(
-                session, localRepo));
-
-        //session.setTransferListener(new ConsoleTransferListener());
-        //session.setRepositoryListener(new ConsoleRepositoryListener());
-
-        // uncomment to generate dirty trees
-        // session.setDependencyGraphTransformer( null );
-
-        return session;
-    }
-
-//    public static RemoteRepository newCentralRepository() {
-//        return new RemoteRepository.Builder("central", "default",
-//                "http://repo1.maven.org/maven2/").build();
-//    }
-
-    // public List<Artifact> resolve(Artifact root,
-    // String scope) throws DependencyResolutionException {
-    // final DependencyFilter filter =
-    // DependencyFilterUtils.classpathFilter(scope);
-    // if (filter == null) {
-    // throw new IllegalStateException(
-    // String.format("failed to create a filter for '%s'", scope)
-    // );
-    // }
-    // return this.resolve(root, scope, filter);
-    // }
-    //
-    // public List<Artifact> resolve( final Artifact root,
-    // final String scope, final DependencyFilter filter)
-    // throws DependencyResolutionException {
-    // final Dependency rdep = new Dependency(root, scope);
-    // final CollectRequest crq = this.request(rdep);
-    // final List<Artifact> deps = new LinkedList<Artifact>();
-    // deps.addAll(
-    // this.fetch(
-    // this.session(),
-    // new DependencyRequest(crq, filter)
-    // )
-    // );
-    // return deps;
-    // }
-    //
-    //
-    // private CollectRequest request(final Dependency root) {
-    // final CollectRequest request = new CollectRequest();
-    // request.setRoot(root);
-    // for (RemoteRepository repo : this.remotes) {
-    // if (!repo.getProtocol().matches("https?|file|s3")) {
-    // Logger.warn(
-    // this,
-    // "%s ignored (only S3, HTTP/S, and FILE are supported)",
-    // repo
-    // );
-    // continue;
-    // }
-    // request.addRepository(repo);
-    // }
-    // return request;
-    // }
-    //
-    //
-    // /**
-    // * Create RepositorySystemSession.
-    // * @return The session
-    // */
-    // private RepositorySystemSession session() {
-    // final LocalRepository local = new LocalRepository(this.localRepo);
-    // final MavenRepositorySystemSession session =
-    // new MavenRepositorySystemSession();
-    // session.setLocalRepositoryManager(
-    // repoSession.newLocalRepositoryManager(local)
-    // );
-    // session.setTransferListener(new LogTransferListener());
-    // return session;
-    // }
-    //
-    //
-    // private List<Artifact> fetch(final RepositorySystemSession session,
-    // final DependencyRequest dreq) throws DependencyResolutionException {
-    // final List<Artifact> deps = new LinkedList<Artifact>();
-    // try {
-    // Collection<ArtifactResult> results;
-    // synchronized (this.localRepo) {
-    // results = this.system
-    // .resolveDependencies(session, dreq)
-    // .getArtifactResults();
-    // }
-    // for (ArtifactResult res : results) {
-    // deps.add(res.getArtifact());
-    // }
-    // // @checkstyle IllegalCatch (1 line)
-    // } catch (Exception ex) {
-    // throw new DependencyResolutionException(
-    // new DependencyResult(dreq),
-    // new IllegalArgumentException(
-    // Logger.format(
-    // "failed to load '%s' from %[list]s into %s",
-    // dreq.getCollectRequest().getRoot(),
-    // Aether.reps(dreq.getCollectRequest().getRepositories()),
-    // session.getLocalRepositoryManager()
-    // .getRepository()
-    // .getBasedir()
-    // ),
-    // ex
-    // )
-    // );
-    // }
-    // return deps;
-    // }
-    //
 
 }
