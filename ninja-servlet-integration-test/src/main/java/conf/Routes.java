a0eb201efcfa71e1df247ed0cdca94d34c770ff6
Mikko Oksa <m3tacity@gmail.com>
2014-02-04 16:10:04 +0200

    Remove GET route (no view for it)

--- ninja-servlet-integration-test/src/main/java/conf/Routes.java
+++ ninja-servlet-integration-test/src/main/java/conf/Routes.java
@@ -95,7 +95,6 @@ public class Routes implements ApplicationRoutes {
         router.GET().route("/contactForm").with(ApplicationController.class, "contactForm");
         router.POST().route("/contactForm").with(ApplicationController.class, "postContactForm");
         
-        router.GET().route("/form").with(ApplicationController.class, "form");
         router.POST().route("/form").with(ApplicationController.class, "postForm");
 
         // /////////////////////////////////////////////////////////////////////
