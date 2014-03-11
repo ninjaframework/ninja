71fe25594ba987bf086e7a89ed5ed29014bbae34
Huang Teng Shiu <smallufo@gmail.com>
2014-03-11 13:01:42 +0800

    Modify NinjaRunner to make use of injector's built-in injectMembers(obj)

--- ninja-test-utilities/src/main/java/ninja/NinjaRunner.java
+++ ninja-test-utilities/src/main/java/ninja/NinjaRunner.java
@@ -16,15 +16,12 @@
 
 package ninja;
 
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 import ninja.utils.NinjaTestServer;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Field;
 
 /**
  * <p>
@@ -78,27 +75,8 @@ public class NinjaRunner extends BlockJUnit4ClassRunner {
     @Override
     protected Object createTest() throws Exception {
         Object testObj = super.createTest();
-
-        for (Field field : testObj.getClass().getDeclaredFields()) {
-            if (field.isAnnotationPresent(Inject.class) || field.isAnnotationPresent(javax.inject.Inject.class)) {
-                Object value = injector.getInstance(field.getType());
-                logger.debug("try to inject {} to {}", value, testObj);
-                try {
-                    if (field.isAccessible()) {
-                        field.set(testObj, value);
-                    } else {
-                        field.setAccessible(true);
-                        field.set(testObj, value);
-                        field.setAccessible(false);
-                    }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+        injector.injectMembers(testObj);
         return testObj;
     }
-
 }
 
