13a17223758bb5f1600915fa8420e3d1867280e7
reyez <raphael.andre.bauer@gmail.com>
2014-04-06 14:32:54 +0200

    fixed flaky testcase.

--- ninja-servlet-jpa-blog-integration-test/src/test/java/controllers/ApplicationControllerTest.java
+++ ninja-servlet-jpa-blog-integration-test/src/test/java/controllers/ApplicationControllerTest.java
@@ -53,7 +53,7 @@ public class ApplicationControllerTest extends NinjaTest {
 
         // If the redirect has worked we must see the following text
         // from the index screen:
-        assertTrue(result.contains("Hello to the blog example!"));
+        assertTrue(result.contains("second"));
 
     }
 
