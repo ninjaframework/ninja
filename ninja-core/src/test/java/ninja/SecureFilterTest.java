1cc0bcd8af4188e8e36dd5b8c8a0fdd1058aea8f
reyez <raphael.andre.bauer@gmail.com>
2014-04-10 14:47:52 +0200

    Fixed imports and added test to make sure USERNAME in SecureFilter is "username".

--- ninja-core/src/test/java/ninja/SecureFilterTest.java
+++ ninja-core/src/test/java/ninja/SecureFilterTest.java
@@ -1,19 +1,18 @@
 /**
  * Copyright (C) 2012-2014 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package ninja;
 
 import static org.junit.Assert.assertEquals;
@@ -22,6 +21,8 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import ninja.session.Session;
 import ninja.utils.NinjaConstant;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,15 +35,12 @@ public class SecureFilterTest {
 
     @Mock
     private Context context;
-    
+
     @Mock
     private Session sessionCookie;
 
-    @Mock 
+    @Mock
     private FilterChain filterChain;
-    
-    @Mock 
-    private Result result;
 
     SecureFilter secureFilter;
 
@@ -55,23 +53,23 @@ public class SecureFilterTest {
     @Test
     public void testSecureFilter() {
 
-        when(context.getSessionCookie()).thenReturn(null);
+        when(context.getSession()).thenReturn(null);
 
         // filter that
         secureFilter.filter(filterChain, context);
 
         verifyZeroInteractions(filterChain);
     }
-    
+
     @Test
     public void testSessionIsNotReturingWhenUserNameMissing() {
 
-        when(context.getSessionCookie()).thenReturn(sessionCookie);
-        when(sessionCookie.get("username")).thenReturn(null);
-        
+        when(context.getSession()).thenReturn(sessionCookie);
+        when(sessionCookie.get(SecureFilter.USERNAME)).thenReturn(null);
+
         // filter that
         Result result = secureFilter.filter(filterChain, context);
-        
+
         assertEquals(NinjaConstant.LOCATION_VIEW_FTL_HTML_FORBIDDEN, result.getTemplate());
         verifyZeroInteractions(filterChain);
     }
@@ -79,13 +77,20 @@ public class SecureFilterTest {
     @Test
     public void testWorkingSessionWhenUsernameIsThere() {
 
-        when(context.getSessionCookie()).thenReturn(sessionCookie);
-        when(sessionCookie.get("username")).thenReturn("myname");
-        
+        when(context.getSession()).thenReturn(sessionCookie);
+        when(sessionCookie.get(SecureFilter.USERNAME)).thenReturn("myname");
+
         // filter that
         secureFilter.filter(filterChain, context);
 
         verify(filterChain).next(context);
+    }
+
+    @Test
+    public void testThatUSERNAMEIsCorrect() {
+
+        assertThat(SecureFilter.USERNAME, equalTo("username"));
+
     }
 
 }
