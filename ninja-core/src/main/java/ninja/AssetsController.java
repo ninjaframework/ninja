f289545eeba792f54320afdfb6975d791f4c386e
Björn 'manf' Heinrichs <manf@derpymail.org>
2015-06-25 18:59:39 +0200

    Added new Tests to AssetsController to keep platform independence Added PowerMock to test libs (required for new mocks)

--- ninja-core/src/main/java/ninja/AssetsController.java
+++ ninja-core/src/main/java/ninja/AssetsController.java
@@ -28,7 +28,6 @@ import java.net.URLConnection;
 
 import ninja.utils.HttpCacheToolkit;
 import ninja.utils.MimeTypes;
-import ninja.utils.NinjaConstant;
 import ninja.utils.NinjaProperties;
 import ninja.utils.ResponseStreams;
 
@@ -36,7 +35,6 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
 import com.google.common.io.ByteStreams;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
--- ninja-core/src/test/java/ninja/AssetsControllerTest.java
+++ ninja-core/src/test/java/ninja/AssetsControllerTest.java
@@ -17,9 +17,12 @@
 package ninja;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyString;
 
 import java.io.ByteArrayOutputStream;
 
@@ -27,6 +30,8 @@ import ninja.utils.HttpCacheToolkit;
 import ninja.utils.MimeTypes;
 import ninja.utils.NinjaProperties;
 import ninja.utils.ResponseStreams;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.junit.Before;
 
 import org.junit.Test;
@@ -35,9 +40,13 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(FilenameUtils.class)
 public class AssetsControllerTest {
 
     @Mock
@@ -257,5 +266,21 @@ public class AssetsControllerTest {
         assertEquals("dir2/file.test", assetsController.normalizePathWithoutLeadingSlash("/dir1/../dir2/file.test", true));
         assertEquals(null, assetsController.normalizePathWithoutLeadingSlash(null, true));
         assertEquals("", assetsController.normalizePathWithoutLeadingSlash("", true));
+        
+        // enforcing test for unix separator
+        mockStatic(FilenameUtils.class, Mockito.CALLS_REAL_METHODS);
+        
+        when(FilenameUtils.normalize(anyString())).then(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments(); 
+                String file = (String) args[0];
+                return FilenameUtils.normalize(file, false);
+            }
+        });
+        
+        assertEquals("\\dir1\\test.test", assetsController.normalizePathWithoutLeadingSlash("/dir1/test.test", !SystemUtils.IS_OS_WINDOWS));
+        assertNotEquals("\\dir1\\test.test", assetsController.normalizePathWithoutLeadingSlash("/dir1/test.test", SystemUtils.IS_OS_WINDOWS));
+        verifyStatic();
     }
 }
