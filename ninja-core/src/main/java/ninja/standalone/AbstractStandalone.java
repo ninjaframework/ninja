5893f16405681737efaa3349f58225f9c3a7a9d0
Joe Lauer <joe@lauer.bz>
2018-01-26 22:45:18 -0500

    Support for running a Ninja console (server-free) application

--- ninja-core/src/main/java/ninja/standalone/AbstractStandalone.java
+++ ninja-core/src/main/java/ninja/standalone/AbstractStandalone.java
@@ -22,19 +22,9 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import ninja.utils.NinjaConstant;
 import ninja.utils.NinjaMode;
-import ninja.utils.NinjaModeHelper;
-import ninja.utils.NinjaPropertiesImpl;
-import ninja.utils.OverlayedNinjaProperties;
-
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.inject.CreationException;
-
 import javax.net.ssl.SSLContext;
 
 /**
@@ -45,14 +35,9 @@ import javax.net.ssl.SSLContext;
  * specific to your Standalone.  See NinjaJetty for example concrete implementation.
  * @param <T> The concrete standalone implementation
  */
-abstract public class AbstractStandalone<T extends AbstractStandalone> implements Standalone<T>, Runnable {
-    // allow logger to take on persona of concrete class
-    final protected Logger logger = LoggerFactory.getLogger(this.getClass());
-
+abstract public class AbstractStandalone<T extends AbstractStandalone> extends AbstractConsole<T> implements Standalone<T>, Runnable {
+    
     // can all be changed prior to configure()
-    protected NinjaMode ninjaMode;
-    protected String externalConfigurationPath;
-    protected String name;
     protected String host;
     protected Integer port;
     protected String contextPath;
@@ -62,21 +47,51 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
     protected String sslKeystorePassword;
     protected URI sslTruststoreUri;
     protected String sslTruststorePassword;
-    
     // internal state
-    protected boolean configured;
-    protected boolean started;
-    protected NinjaPropertiesImpl ninjaProperties;                      // after configure()
-    protected OverlayedNinjaProperties overlayedNinjaProperties;        // after configure()
     protected List<String> serverUrls;
     protected List<String> baseUrls;
     
     public AbstractStandalone(String name) {
-        // set mode as quickly as possible (can still be changed before configure())
-        this.ninjaMode = NinjaModeHelper.determineModeFromSystemPropertiesOrProdIfNotSet();
-        this.name = name;
-        this.configured = false;
-        this.started = false;
+        super(name);
+    }
+    
+    @Override
+    protected void doPreConfigure() throws Exception {
+        // current value or system property or conf/application.conf or default value
+        host(overlayedNinjaProperties.get(Standalone.KEY_NINJA_HOST, this.host, Standalone.DEFAULT_HOST));
+        port(overlayedNinjaProperties.getInteger(Standalone.KEY_NINJA_PORT, this.port, Standalone.DEFAULT_PORT));
+        contextPath(overlayedNinjaProperties.get(Standalone.KEY_NINJA_CONTEXT_PATH, this.contextPath, Standalone.DEFAULT_CONTEXT_PATH));
+        idleTimeout(overlayedNinjaProperties.getLong(Standalone.KEY_NINJA_IDLE_TIMEOUT, this.idleTimeout, Standalone.DEFAULT_IDLE_TIMEOUT));
+        sslPort(overlayedNinjaProperties.getInteger(Standalone.KEY_NINJA_SSL_PORT, this.sslPort, Standalone.DEFAULT_SSL_PORT));
+        // defaults below (with self-signed cert) only valid in dev & test modes
+        sslKeystoreUri(overlayedNinjaProperties.getURI(Standalone.KEY_NINJA_SSL_KEYSTORE_URI, this.sslKeystoreUri, this.ninjaMode == NinjaMode.prod ? null : new URI(Standalone.DEFAULT_DEV_NINJA_SSL_KEYSTORE_URI)));
+        sslKeystorePassword(overlayedNinjaProperties.get(Standalone.KEY_NINJA_SSL_KEYSTORE_PASSWORD, this.sslKeystorePassword, this.ninjaMode == NinjaMode.prod ? null : Standalone.DEFAULT_DEV_NINJA_SSL_KEYSTORE_PASSWORD));
+        sslTruststoreUri(overlayedNinjaProperties.getURI(Standalone.KEY_NINJA_SSL_TRUSTSTORE_URI, this.sslTruststoreUri, this.ninjaMode == NinjaMode.prod ? null : new URI(Standalone.DEFAULT_DEV_NINJA_SSL_TRUSTSTORE_URI)));
+        sslTruststorePassword(overlayedNinjaProperties.get(Standalone.KEY_NINJA_SSL_TRUSTSTORE_PASSWORD, this.sslTruststorePassword, this.ninjaMode == NinjaMode.prod ? null : Standalone.DEFAULT_DEV_NINJA_SSL_TRUSTSTORE_PASSWORD));
+        // assign random ports if needed
+        if (getPort() == null || getPort() == 0) {
+            port(StandaloneHelper.findAvailablePort(8000, 9000));
+        }
+        if (getSslPort() == null || getSslPort() == 0) {
+            sslPort(StandaloneHelper.findAvailablePort(9001, 9999));
+        }
+    }
+    
+    @Override
+    protected void doPostConfigure() throws Exception {
+        // build configured urls
+        this.serverUrls = createServerUrls();
+        this.baseUrls = createBaseUrls();
+        // is there at least one url?
+        if (this.serverUrls == null || this.serverUrls.isEmpty()) {
+            throw new IllegalStateException("All server ports were disabled." + " Check the 'ninja.port' property and possibly others depending your standalone.");
+        }
+        // save generated server name as ninja property if its not yet set
+        String serverName = this.ninjaProperties.get(NinjaConstant.serverName);
+        if (StringUtils.isEmpty(serverName)) {
+            // grab the first one
+            this.ninjaProperties.setProperty(NinjaConstant.serverName, getServerUrls().get(0));
+        }
     }
     
     /**
@@ -117,97 +132,6 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
     }
     
     @Override
-    final public T configure() throws Exception {
-        checkNotConfigured();
-        
-        // create ninja properties & overlayed view
-        this.ninjaProperties = new NinjaPropertiesImpl(this.ninjaMode, this.externalConfigurationPath);
-        
-        this.overlayedNinjaProperties = new OverlayedNinjaProperties(this.ninjaProperties);
-        
-        // current value or system property or conf/application.conf or default value
-        host(overlayedNinjaProperties.get(
-                KEY_NINJA_HOST, this.host, DEFAULT_HOST));
-        
-        port(overlayedNinjaProperties.getInteger(
-                KEY_NINJA_PORT, this.port, DEFAULT_PORT));
-        
-        contextPath(overlayedNinjaProperties.get(
-                KEY_NINJA_CONTEXT_PATH, this.contextPath, DEFAULT_CONTEXT_PATH));
-        
-        idleTimeout(overlayedNinjaProperties.getLong(
-                KEY_NINJA_IDLE_TIMEOUT, this.idleTimeout, DEFAULT_IDLE_TIMEOUT));
-        
-        sslPort(overlayedNinjaProperties.getInteger(
-                KEY_NINJA_SSL_PORT, this.sslPort, DEFAULT_SSL_PORT));
-        
-        // defaults below (with self-signed cert) only valid in dev & test modes
-        
-        sslKeystoreUri(overlayedNinjaProperties.getURI(KEY_NINJA_SSL_KEYSTORE_URI, this.sslKeystoreUri, 
-                (this.ninjaMode == NinjaMode.prod ? null : new URI(DEFAULT_DEV_NINJA_SSL_KEYSTORE_URI))));
-        
-        sslKeystorePassword(overlayedNinjaProperties.get(
-                KEY_NINJA_SSL_KEYSTORE_PASSWORD, this.sslKeystorePassword, 
-                (this.ninjaMode == NinjaMode.prod ? null : DEFAULT_DEV_NINJA_SSL_KEYSTORE_PASSWORD)));
-        
-        sslTruststoreUri(overlayedNinjaProperties.getURI(KEY_NINJA_SSL_TRUSTSTORE_URI, this.sslTruststoreUri, 
-                (this.ninjaMode == NinjaMode.prod ? null : new URI(DEFAULT_DEV_NINJA_SSL_TRUSTSTORE_URI))));
-        
-        sslTruststorePassword(overlayedNinjaProperties.get(
-                KEY_NINJA_SSL_TRUSTSTORE_PASSWORD, this.sslTruststorePassword, 
-                (this.ninjaMode == NinjaMode.prod ? null : DEFAULT_DEV_NINJA_SSL_TRUSTSTORE_PASSWORD)));
-        
-        
-        // assign random ports if needed
-        if (getPort() == null || getPort() == 0) {
-            port(StandaloneHelper.findAvailablePort(8000, 9000));
-        }
-        
-        if (getSslPort() == null || getSslPort() == 0) {
-            sslPort(StandaloneHelper.findAvailablePort(9001, 9999));
-        }
-        
-        doConfigure();
-        
-        this.configured = true;
-        
-        // build configured urls
-        this.serverUrls = createServerUrls();
-        this.baseUrls = createBaseUrls();
-        
-        // is there at least one url?
-        if (this.serverUrls == null || this.serverUrls.isEmpty()) {
-            throw new IllegalStateException("All server ports were disabled." + 
-                    " Check the 'ninja.port' property and possibly others depending your standalone.");
-        }
-        
-        // save generated server name as ninja property if its not yet set
-        String serverName = this.ninjaProperties.get(NinjaConstant.serverName);
-        
-        if (StringUtils.isEmpty(serverName)) {
-            // grab the first one
-            this.ninjaProperties.setProperty(NinjaConstant.serverName, getServerUrls().get(0));
-        }
-        
-        return (T)this;
-    }
-    
-    @Override
-    final public T start() throws Exception {
-        if (!this.configured) {
-            configure();
-        }
-        
-        doStart();
-        
-        this.started = true;
-        
-        logBaseUrls();
-        
-        return (T)this;
-    }
-    
-    @Override
     final public T join() throws Exception{
         checkStarted();
         
@@ -216,72 +140,7 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
         return (T)this;
     }
     
-    @Override
-    final public T shutdown() {
-        doShutdown();
-        return (T)this;
-    }
-    
-    abstract protected void doConfigure() throws Exception;
-    
-    abstract protected void doStart() throws Exception;
-    
     abstract protected void doJoin() throws Exception;
-    
-    abstract protected void doShutdown();
-    
-    protected void checkNotConfigured() {
-        if (this.configured) {
-            throw new IllegalStateException(this.getClass().getCanonicalName() + ".configure() already called");
-        }
-    }
-    
-    protected void checkConfigured() {
-        if (!this.configured) {
-            throw new IllegalStateException(this.getClass().getCanonicalName() + ".configure() not called yet");
-        }
-    }
-    
-    protected void checkStarted() {
-        if (!this.started) {
-            throw new IllegalStateException(this.getClass().getCanonicalName() + ".start() not called yet");
-        }
-    }
-    
-    // semi-builder pattern
-    
-    @Override
-    public NinjaMode getNinjaMode() {
-        return ninjaMode;
-    }
-    
-    @Override
-    public T ninjaMode(NinjaMode ninjaMode) {
-        this.ninjaMode = ninjaMode;
-        return (T)this;
-    }
-    
-    @Override
-    public String getExternalConfigurationPath() {
-        return this.externalConfigurationPath;
-    }
-    
-    @Override
-    public T externalConfigurationPath(String externalConfigurationPath) {
-        this.externalConfigurationPath = externalConfigurationPath;
-        return (T)this;
-    }
-    
-    @Override
-    public String getName() {
-        return name;
-    }
-    
-    @Override
-    public T name(String name) {
-        this.name = name;
-        return (T)this;
-    }
     
     @Override
     public Integer getPort() {
@@ -384,13 +243,6 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
     }
     
     @Override
-    public NinjaPropertiesImpl getNinjaProperties() {
-        // only available after configure()
-        checkConfigured();
-        return ninjaProperties;
-    }
-    
-    @Override
     public List<String> getServerUrls() {
         // only available after configure()
         checkConfigured();
@@ -477,14 +329,6 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
         return sb.toString();
     }
     
-    protected Exception tryToUnwrapInjectorException(Exception exception) {
-        Throwable cause = exception.getCause();
-        if (cause != null && cause instanceof CreationException) {
-            return (CreationException)cause;
-        } else {
-            return exception;
-        }
-    }
     
     protected String getLoggableIdentifier() {
         // build list of ports
@@ -513,13 +357,16 @@ abstract public class AbstractStandalone<T extends AbstractStandalone> implement
         return s.toString();
     }
     
-    protected void logBaseUrls() {
+    @Override
+    protected void logStarted() {
         logger.info("-------------------------------------------------------");
         logger.info("Ninja application running at");
         
-        List<String> uris = getBaseUrls();
-        for (String uri : uris) {
-            logger.info(" => {}", uri);
+        List<String> uris = this.getBaseUrls();
+        if (uris != null) {
+            uris.forEach(uri -> {
+                logger.info(" => {}", uri);
+            });
         }
         
         logger.info("-------------------------------------------------------");
---
+++ ninja-core/src/site/markdown/documentation/console.md
@@ -0,0 +1,47 @@
+Console Application
+===================
+
+## Introduction
+
+Ninja is designed to be a web framework, but if you want to leverage its 
+excellent dependency injection or configuration for running a webserver-less
+console application, Ninja v6.3.0+ added support for `ninja.standalone.NinjaConsole`
+as a main entry point.
+
+Your application will start like it normally would if running in a web server
+container like Jetty, but will not start a web server and will not initialize
+your `conf.Router`. All other Guice configuration and lifecycle methods will
+be started as they normally would if running as a standard Ninja web application.
+
+## Usage
+
+You can either start the JVM directly or customize `ninja.standalone.NinjaConsole`.
+Since it includes a main method, the following would start your console-based
+Ninja application:
+
+<pre class="prettyprint">
+java -cp &lt;classpath-here&gt; ninja.standalone.NinjaConsole
+</pre>
+
+Or you can write your own main method and customize `NinjaConsole` as much
+as you need:
+
+<pre class="prettyprint">
+import ninja.standalone.NinjaConsole;
+import ninja.utils.NinjaMode;
+
+public class MyMain {
+ 
+    static public void main(String[] args) throws Exception {
+        NinjaConsole ninja = new NinjaConsole()
+            .ninjaMode(NinjaMode.prod)
+            .start();
+
+        // other code (e.g. access guice injector)
+        // ninja.getInjector();
+
+        ninja.shutdown();
+    }
+    
+}
+</pre>
