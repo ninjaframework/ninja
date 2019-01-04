e7ea47a764a30989d96efa1d13e920dea674a72b
Raphael A. Bauer <r@raphaelbauer.com>
2019-01-04 13:14:00 +0100

    Hello 2019

--- ninja-servlet-jpa-blog-archetype/src/main/resources/archetype-resources/src/main/java/dao/UserDao.java
+++ ninja-servlet-jpa-blog-archetype/src/main/resources/archetype-resources/src/main/java/dao/UserDao.java
@@ -3,7 +3,7 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /**
- * Copyright (C) 2012-2018 the original author or authors.
+ * Copyright (C) 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
