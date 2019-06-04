2c21c21c82f50a6263f185f5ae5b8345ed59b2a5
Zalan Meggyesi <zmeggyesi@skawa.hu>
2019-06-04 18:12:37 +0200

    More fun with modules

--- ninja-standalone/src/main/java/module-info.java
+++ ninja-standalone/src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module ninja.standalone {
 	requires com.google.guice;
-	requires jetty.server;
+	requires org.eclipse.jetty.servlet;
+	requires org.eclipse.jetty.server;
+	requires org.eclipse.jetty.http;
+	requires org.eclipse.jetty.util;
+	requires org.eclipse.jetty.websocket.javax.websocket.server;
 	requires ninja.servlet;
-	requires jetty.servlet;
-	requires javax.servlet.api;
-	requires jetty.http;
-	requires jetty.util;
-	requires jetty.xml;
-	requires slf4j.api;
 	requires ninja.core;
-	requires commons.lang3;
 	requires java.xml;
+	requires javax.servlet.api;
+	requires slf4j.api;
+	requires commons.lang3;
+	requires org.eclipse.jetty.xml;
 
 	exports standalone;
 	exports standalone.console;
