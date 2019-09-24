15cdaad048d84313d57e4d5cef26d9ec841f87da
Giacomo Lacava <g.lacava@gmail.com>
2019-09-24 23:52:33 +0100

    Override logo if ninja/logo.txt is present.

--- ninja-core/src/main/java/ninja/NinjaDefault.java
+++ ninja-core/src/main/java/ninja/NinjaDefault.java
@@ -16,12 +16,17 @@
 
 package ninja;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
 
 import javax.management.RuntimeErrorException;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import ninja.diagnostics.DiagnosticError;
 import ninja.diagnostics.DiagnosticErrorBuilder;
 import ninja.exceptions.BadRequestException;
@@ -477,9 +482,16 @@ public class NinjaDefault implements Ninja {
     private final void showSplashScreenViaLogger() {
         
         String ninjaVersion = readNinjaVersion();
-        
-        // log Ninja splash screen
-        logger.info(NINJA_LOGO, ninjaVersion);
+
+        // log Ninja splash screen, from resources if available (so it can be overridden)
+        String NINJA_LOGO_LOCATION = "ninja/logo.txt";
+        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(NINJA_LOGO_LOCATION);) {
+            InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(is), Charsets.UTF_8);
+            logger.info(CharStreams.toString(reader), ninjaVersion);
+        } catch (IOException | NullPointerException e) {
+            // if anything happens, use the old one
+            logger.info(NINJA_LOGO, ninjaVersion);
+        }
         
     }
 
